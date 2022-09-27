b3799bbd99ba6e6e629c514c7a135d1808accc97
Esteban <esteban.c.ojedahdez@gmail.com>
2022-09-27 17:18:25 +0100

    Añadido un comentario a scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,7 @@
 // JavaScript to be added
+
+/*Como todavía no sabemos programar un prompt para 
+darle la opcion a un usuario de suscribirse a nuestra
+mail list, escribimos este comentario para dejar 
+constancia de los cambios hechos mediante fork y 
+merge*/
